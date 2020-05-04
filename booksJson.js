c6ef10b6b47f302ca6718b867c229c3587661a36
ntlokoo <Odwa.Ntloko@vodacom.co.za>
2020-05-04 16:12:43 +0200

    installed mongoDB and wrote to it

---
+++ booksJson.js
@@ -0,0 +1,32 @@
+db.books.insert([
+    {
+        title: 'War and Peace',
+        genre: 'Historical Fiction',
+        author: 'Sum One',
+        read: false
+    },
+    {
+        title: 'Ar and Peace',
+        genre: 'Historical Fictio',
+        author: 'Sum Onee',
+        read: false
+    }, 
+    {
+        title: 'R and Peace',
+        genre: 'Historically Fiction',
+        author: 'Summ One',
+        read: false
+    }, 
+    {
+        title: 'Wr and Peace',
+        genre: 'Histrical Fiction',
+        author: 'Sm One',
+        read: false
+    },
+    {
+        title: 'ar and eace',
+        genre: 'Histical Ficion',
+        author: 'Sum On',
+        read: false
+    }
+])
