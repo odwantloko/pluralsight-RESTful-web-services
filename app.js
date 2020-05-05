c1cef70e682ae8f4218db9cf2eec80dde143aea1
ntlokoo <Odwa.Ntloko@vodacom.co.za>
2020-05-05 12:24:29 +0200

    implemented post method

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 const bookRouter = express.Router();
-
 const port = process.env.PORT || 3000;
-const Book = require('./models/bookModel')
+const Book = require('./models/bookModel');
+//const bookRouter = require('./routes/bookRouter')
 
-// GET request which returns a single item
-bookRouter.route('/books/:bookId')
-  .get((req,res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if(err){
-        return res.send(err);
-      }
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
 
-      return res.json(book)
-      
-    });
+bookRouter.route('/books')
+  // POST method which writes to the db
+  .post((req, res) => {
+      const book = new Book(req.body);
 
-  });
-
+      book.save(); // saves the data to mongoDB
+      return res.status(201).json(book); //response which can be seen through insomnia/postman etc.
+  })
+  
   // GET request which returns a list of books
-  bookRouter.route('/books')
   .get((req,res) => {
     const query = {};
 
@@ -44,8 +42,22 @@ bookRouter.route('/books/:bookId')
   });
 
 
-app.use('/api', bookRouter);
-  
+bookRouter.route('/books/:bookId')
+  // GET request which returns a single item
+  .get((req,res) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if(err){
+        return res.send(err);
+      }
+
+      return res.json(book)
+      
+    });
+
+  });
+
+
+app.use('/api', bookRouter); 
 
 app.get('/', (req, res) => {
   res.send('Welcome to my nodemon API');
